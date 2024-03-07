5d180143d2529ad49b38b2d7acf349d09c7957f7
gemstorm <123022401+gem-storm@users.noreply.github.com>
2024-03-07 15:21:04 -0800

    presets, logging, suffix customization
    -presets. edit the dict at the top of the script to customize.
    
    -logging. add -log to your arguments to write the ffmpeg commands to a file.
    
    -suffix customization. add -suffix "XYZ" to customize the output filename suffix.

--- README.md
+++ README.md
@@ -1,14 +1,20 @@
 ## sendto-ffmpeg
 tired of typing output path(s) for ffmpeg? try using sendto-ffmpeg!
 
-### usage
-right click your video(s) and select `Send to -> ffmpeg`. a window will open, you type your arguments in, and it'll automatically make the output path for you!
 
-you can also add `-container XYZ` if you would like to use a different container.
+### usage
+right click your video(s) and select `Send to -> ffmpeg`. a window will open, you type your arguments in, and it'll automatically run the command for you! (no need for typing any paths!)
+
 
 ### dependencies
-- python
-- ffmpeg in path
+- python (preferably in path)
+- ffmpeg (must be in path)
+you can install both of these easily using [scoop](https://scoop.sh):
+```powershell
+scoop.cmd install python
+scoop.cmd install ffmpeg
+```
+
 
 ### installation
 1. download `main.py`
@@ -19,3 +25,26 @@ you can also add `-container XYZ` if you would like to use a different container
 3. name the shortcut to whatever you'd like, i call it `ffmpeg`
 4. move the shortcut to the sendto folder (`%appdata%/microsoft/windows/sendto`)
 5. done!
+
+
+### misc. arguments
+1. add `-container "XYZ"` while typing your arguments to specify the output container. e.g. `-container "mkv"`. default depends on codec.
+2. adding `-log` will write the ffmpeg command(s) to a file called `log.txt` in the sendto folder.
+3. adding `-suffix "XYZ"` will change the suffix applied to the filename. e.g. `-suffix " - h264"`. defaults to ` ~ Encoded`.
+
+
+### presets
+at the top of `main.py`, you can add your own argument presets, for example:
+```py
+presets = {
+    "H264": "-c:v libx264 -preset slow -aq-mode 3 -crf 16",
+}
+```
+a few are included by default. if the value before `:` is in your args, it will automatically be replaced with the value after `:`.
+
+you can also make a preset called `"DEFAULT"` which will apply if no arguments are given.
+
+syntax:
+- it's recommended to keep the first value in all caps, so regular arguments aren't replaced by accident.
+- make sure to end each line with `,` or python won't recognize the ones following (and will prob crash).
+- make sure all keys and values are surrounded by quotes (`'` or `"`).
--- main.py
+++ main.py
@@ -1,6 +1,24 @@
+presets = {
+    "H264": "-c:v libx264 -preset slow -aq-mode 3 -crf 16",
+    "HEVC": "-c:v libx265 -preset medium -x265-params aq-mode=3:no-sao=1 -crf 20",
+    "UTVIDEO": "-c:v utvideo",
+    "4K": "-vf scale=3840:-2:flags=neighbor",
+    "FLAC": "-c:a flac -container flac",
+    "MP3": "-c:a mp3 -b:a 320k -container mp3",
+    "ACOPY": "-c:a copy",
+    "VCOPY": "-c:v copy",
+    "DEFAULT": "-c:v libx264 -preset slow -aq-mode 3 -crf 16 -c:a copy",
+}
+
+# add your own! make sure to end each line with a comma (,)
+# keep them in ALL CAPS so regular arguments don't get replaced
+# "DEFAULT" will automatically apply if no arguments are typed
+
+
 from os.path import splitext
 from subprocess import run
 from sys import argv
+
 
 if len(argv) == 1:
     vids = input(
@@ -14,19 +32,51 @@ else:
 def container(ffmpeg_args):
     if "-container" in ffmpeg_args:
         split_args = ffmpeg_args.split("-container ", 2)[1].strip()
-        return split_args.split(" ", 1)[0]
+        return split_args.split(" ", 1)[0].replace('"', "")
     elif "utvideo" in ffmpeg_args:
         return "mkv"
     return "mp4"
 
 
-def remove_container(ffmpeg_args):
-    return ffmpeg_args.replace("-container " + container(ffmpeg_args), "")
+def log(ffmpeg_args):
+    if "-log" in ffmpeg_args:
+        return True
+    return False
+
+
+def suffix(ffmpeg_args):
+    if "-suffix" in ffmpeg_args:
+        split_args = ffmpeg_args.split("-suffix", 2)[1].strip()
+        return " ".join(split_args.split(" ")[:-1]).replace('"', "")
+    return "~ Encoded"
+
+
+def preset(ffmpeg_args, presets):
+    for key, value in presets.items():
+        if key in ffmpeg_args:
+            ffmpeg_args = ffmpeg_args.replace(key, value)
+    return ffmpeg_args
+
+
+def fix_args(args_preset):
+    args_suffix = args_preset.replace(f'-suffix "{suffix(args_preset)}"', "")
+    args_log = args_suffix.replace("-log", "")
+    new_args = args_log.replace(f"-container {container(args_log)}", "")
+    # i cant think of any way to do this better sry
+    if new_args.strip():
+        return new_args
+    return presets["DEFAULT"]
 
 
 enc_args = input("args:\n>>>")
 
 for v in input_videos:
+    args_preset = preset(enc_args, presets)
+    if log(enc_args):
+        with open("log.txt", "w") as file:
+            file.write(
+                f'ffmpeg -i "{v}" {fix_args(args_preset)} "{splitext(v)[0]}{suffix(args_preset)}.{container(args_preset)}"\n'
+            )
     run(
-        f'ffmpeg -i "{v}" {remove_container(enc_args)} "{splitext(v)[0]} ~ Encoded.{container(enc_args)}"'
+        f'ffmpeg -i "{v}" {fix_args(args_preset)} "{splitext(v)[0]}{suffix(args_preset)}.{container(args_preset)}"'
     )
